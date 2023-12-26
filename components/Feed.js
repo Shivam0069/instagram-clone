1bd960c687af9e07270eda42c94243c2fcf0b911
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2023-12-26 15:48:15 +0530

    Added Key to Comments and resolved the error of miniProfile

--- components/Feed.js
+++ components/Feed.js
@@ -10,7 +10,9 @@ export default function Feed() {
   return (
     <main
       className={`grid  ${
-        session ? "grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto" : "grid-cols-1 md:grid-cols-2 md:max-w-3xl mx-auto"
+        session
+          ? "grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto"
+          : "grid-cols-1 md:grid-cols-2 md:max-w-3xl mx-auto"
       }`}
     >
       <section className="md:col-span-2">
@@ -19,15 +21,16 @@ export default function Feed() {
         {/* Posts */}
         <Posts />
       </section>
-
-      <section className="hidden md:inline-grid md:col-span-1">
-        <div className="fixed w-[380px]">
-          {/* Mini Profile */}
-          <MiniProfile />
-          {/* Suggestions */}
-          <Suggestions />
-        </div>
-      </section>
+      {session && (
+        <section className="hidden md:inline-grid md:col-span-1">
+          <div className="fixed w-[380px]">
+            {/* Mini Profile */}
+            <MiniProfile />
+            {/* Suggestions */}
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </main>
   );
 }
