8c9926501db48e7bb84c23a756e59a5b488cf200
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2023-12-26 15:22:31 +0530

    Apply like functionality

--- components/Post.js
+++ components/Post.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
 import Moment from "react-moment";
-import { BookMarkIcon, CommentIcon, HeartIcon, SmileyIcon } from "./SvgIcons";
+import {
+  BookMarkIcon,
+  CommentIcon,
+  HeartIcon,
+  HeartIconFilled,
+  SmileyIcon,
+} from "./SvgIcons";
 import {
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   onSnapshot,
   orderBy,
   query,
   serverTimestamp,
+  setDoc,
 } from "firebase/firestore";
 import { db } from "@/firebase";
 
@@ -16,7 +25,8 @@ export default function Post({ img, userImg, username, caption, id }) {
   const { data: session } = useSession();
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
-
+  const [hasLiked, setHasLiked] = useState(false);
+  const [likes, setLikes] = useState([]);
   useEffect(() => {
     const unsubscribe = onSnapshot(
       query(
@@ -29,6 +39,25 @@ export default function Post({ img, userImg, username, caption, id }) {
     );
     return unsubscribe;
   }, [db, id]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", id, "likes"),
+      (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [db]);
+  useEffect(() => {
+    setHasLiked(likes.findIndex((like) => like.id === session.user.uid) !== -1);
+  }, [likes]);
+  async function likePost() {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session?.user.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session?.user.uid), {
+        username: session.user.username,
+      });
+    }
+  }
+
   async function sendComment(e) {
     e.preventDefault();
     const commentToSend = comment;
@@ -40,6 +69,7 @@ export default function Post({ img, userImg, username, caption, id }) {
       timestamp: serverTimestamp(),
     });
   }
+
   return (
     <div className="bg-white my-7 border rounded-md">
       {/* Post Header */}
@@ -74,7 +104,35 @@ export default function Post({ img, userImg, username, caption, id }) {
       {session && (
         <div className="flex justify-between px-4 pt-4">
           <div className="flex space-x-4 ">
-            {HeartIcon}
+            {hasLiked ? (
+              <svg
+                onClick={likePost}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="btn text-red-400"
+              >
+                <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+              </svg>
+            ) : (
+              <svg
+                onClick={likePost}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="btn"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                />
+              </svg>
+            )}
+            
+
             {CommentIcon}
           </div>
           {BookMarkIcon}
@@ -94,7 +152,11 @@ export default function Post({ img, userImg, username, caption, id }) {
         <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
           {comments.map((comment) => (
             <div className="flex items-center space-x-2 mb-2">
-              <img className="h-7 rounded-full object-cover " src={comment.data().userImage} alt="user-image" />
+              <img
+                className="h-7 rounded-full object-cover "
+                src={comment.data().userImage}
+                alt="user-image"
+              />
               <p className="font-semibold ">{comment.data().username}</p>
               <p className="flex-1 truncate">{comment.data().comment}</p>
               <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
