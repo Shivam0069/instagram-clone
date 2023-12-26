4bb438ef0fda00292972f452c438b5eb44ffde7a
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2023-12-26 22:16:13 +0530

    Editing NextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,37 @@
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+//   secret: process.env.SECRET,
+//   pages: {
+//     signIn: "/auth/signin",
+//   },
+
+//   callbacks: {
+//     async session({ session, token, user }) {
+//       session.user.username = session.user.name
+//         .split(" ")
+//         .join("")
+//         .toLocaleLowerCase();
+//       session.user.uid = token.sub;
+//       return session;
+//     },
+//   },
+// };
+
+// export default NextAuth(authOptions);
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -25,6 +55,4 @@ export const authOptions = {
       return session;
     },
   },
-};
-
-export default NextAuth(authOptions);
+});
